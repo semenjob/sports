0e93de4c8bdec0cf0d7e9be172ccdfd8d2e39be9
Semen Trushik <Semen.trushik.gh@gmail.com>
2022-03-19 19:22:25 +0300

    corrected variables, removed font mixins, adapted 3 blocks in a new way

--- src/js/script.js
+++ src/js/script.js
@@ -2,6 +2,15 @@ $(document).ready(function(){
   $('.carousel_inner').slick({
     prevArrow: '<button type="button" class="slick-prev"><img src="./icons/solid/left-solid.png"></button>',
     nextArrow: '<button type="button" class="slick-next"><img src="./icons/solid/right-solid.png"></button>',
+    responsive: [
+      {
+        breakpoint: 992,
+        settings: {
+          arrows: false,
+          dots: true,
+        }
+      },
+    ]
   });
 });
 	
