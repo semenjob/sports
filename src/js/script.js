f3447451f0d7e8d0b8a0cdb0b8d477e883c9e5a5
Semen Trushik <Semen.trushik.gh@gmail.com>
2022-03-22 18:50:46 +0300

    animation

--- src/js/script.js
+++ src/js/script.js
@@ -49,5 +49,49 @@ toggleSlide('.catalog-item_back');
         $('.overlay, #order').fadeIn('slow'); 
       })
     });
+
+    function validateForms(form){
+      $(form).validate({
+          rules: {
+              name: {
+                  required: true,
+                  minlength: 2
+              },
+              phone: "required",
+              email: {
+                  required: true,
+                  email: true
+              }
+          },
+          messages: {
+              name: {
+                  required: "Пожалуйста, введите свое имя",
+                  minlength: jQuery.validator.format("Введите {0} символа!")
+                },
+              phone: "Пожалуйста, введите свой номер телефона",
+              email: {
+                required: "Пожалуйста, введите свою почту",
+                email: "Неправильно введен адрес почты"
+              }
+          }
+      });
+  };
+
+  validateForms('#consultation-form');
+  validateForms('#consaltion form');
+  validateForms('#order form');
+
+  // $('input[name=phone]').mask('+3 (806) 31 41 53 52');
+  // $('input[name=name]').mask('Semen');
+  // $('input[name=email]').mask('Trushik.Semen@gmail.com');
+
+  $(window).scroll(function(){
+    if ($(this).scrollTop() > 1600) {
+      $('.scrolls').fadeIn();
+    } else {
+      $('.scrolls').fadeOut('slow');
+    }
+  });
+  
 });
 	
